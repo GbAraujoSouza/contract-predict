80f628d23ee06b10672b38d6c320746fc6e10683
GabrielDeAraujoDeSouza <gabriel.souzaaraujo109@gmail.com>
2026-06-29 03:56:08 -0300

    use 5 fold and cleanup code

--- frontend/src/app/models/health-response.model.ts
+++
@@ -1,4 +0,0 @@
-export interface HealthResponse {
-  status: string;
-  message: string;
-}
--- frontend/src/app/pages/info/info.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-info-page',
-  templateUrl: './info.page.html',
-  styleUrl: './info.page.scss'
-})
-export class InfoPage {}
